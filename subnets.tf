f2ea9df01658f0830154c7bfc8096a4f4c994173
tparaoan <tiffany.paraoan@cloudreach>
2022-04-08 12:41:38 -0500

    ADD - update VPC with public b and private b subnets

--- subnets.tf
+++ subnets.tf
@@ -9,6 +9,17 @@ resource "aws_subnet" "public" {
     }
 }
 
+resource "aws_subnet" "public-b" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "192.168.10.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "talent-academy-public-b"
+    }
+}
+
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
     cidr_block = "192.168.2.0/24"
@@ -16,6 +27,16 @@ resource "aws_subnet" "private" {
     
     tags = {
         Name = "talent-academy-private-a"
+    }
+}
+
+resource "aws_subnet" "private-b" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "192.168.20.0/24"
+    availability_zone = "us-east-1b"
+    
+    tags = {
+        Name = "talent-academy-private-b"
     }
 }
 
